344195e2480d5f8e49b07988c8855467ebd23dee
Bohdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2024-01-06 14:48:25 +0200

    Added change of stake

--- src/main/java/elements/GameControls.java
+++ src/main/java/elements/GameControls.java
@@ -9,9 +9,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameControls {
 
     public static Group controls;
+    private static final Map<String, Control> mapControls = new HashMap<>();
     private static final int HEIGHT = 25;
     private static final int MARGIN_SIZE = 10;
 
@@ -26,65 +30,76 @@ public class GameControls {
         grid.setPrefHeight(HEIGHT);
 
         Font font = new Font(HEIGHT);
-        int counter = 0;
 
-        grid.add(getBtnMinus(font), counter++, 0);
-        grid.add(getLblStake(font), counter++, 0);
-        grid.add(getBtnPlus(font), counter++, 0);
-        grid.add(getLblWin(font), counter++, 0);
-        grid.add(getTextWin(font), counter++, 0);
-        grid.add(getSpin(font), counter++, 0);
+        addLblStake(font, grid, 1);
+        addLblWin(font, grid, 3);
+        addBtnMinus(font, grid, 0);
+        addBtnPlus(font, grid, 2);
+        addTextWin(font, grid, 4);
+        addBtnSpin(font, grid, 5);
 
         group.getChildren().add(grid);
 
         return group;
     }
 
-    private static Control getBtnMinus(Font font) {
-        Button button = new Button("-");
-        button.setFont(font);
-
-        return button;
+    private static void addControl(Control control, GridPane grid, String nameControl, int index) {
+        grid.add(control, index, 0);
+        mapControls.put(nameControl, control);
     }
 
-    private static Control getLblStake(Font font) {
-        Label label = new Label("0.0");
-        label.setFont(font);
-        GridPane.setMargin(label, new Insets(MARGIN_SIZE));
+    private static void addLblStake(Font font, GridPane grid, int index) {
+        Label control = new Label();
+        control.setFont(font);
+        GridPane.setMargin(control, new Insets(MARGIN_SIZE));
 
-        return label;
+        Manage.currentBet(control, 100);
+
+        addControl(control, grid, "lblStake", index);
     }
 
-    private static Control getBtnPlus(Font font) {
-        Button button = new Button("+");
-        button.setFont(font);
+    private static void addLblWin(Font font, GridPane grid, int index) {
+        Label control = new Label("Win:");
+        control.setFont(font);
+        GridPane.setMargin(control, new Insets(MARGIN_SIZE));
 
-        return button;
+        addControl(control, grid, "lblWin", index);
     }
 
-    private static Control getLblWin(Font font) {
-        Label label = new Label("Win:");
-        label.setFont(font);
-        GridPane.setMargin(label, new Insets(MARGIN_SIZE));
+    private static void addBtnMinus(Font font, GridPane grid, int index) {
+        Button control = new Button("-");
+        control.setFont(font);
 
-        return label;
+        control.setOnAction(event -> Manage.minusBet((Label) mapControls.get("lblStake")));
+
+        addControl(control, grid, "btnMinus", index);
     }
 
-    private static Control getTextWin(Font font) {
-        TextArea textWin = new TextArea();
-        textWin.setText("0.00");
-        textWin.setPrefWidth(8 * HEIGHT);
-        textWin.setFont(font);
-        textWin.setEditable(false);
-        GridPane.setMargin(textWin, new Insets(MARGIN_SIZE));
 
-        return textWin;
+    private static void addBtnPlus(Font font, GridPane grid, int index) {
+        Button control = new Button("+");
+        control.setFont(font);
+
+        control.setOnAction(event -> Manage.plusBet((Label) mapControls.get("lblStake")));
+
+        addControl(control, grid, "btnPlus", index);
     }
 
-    private static Control getSpin(Font font) {
-        Button button = new Button("SPIN");
-        button.setFont(font);
+    private static void addTextWin(Font font, GridPane grid, int index) {
+        TextArea control = new TextArea();
+        control.setText("0.00");
+        control.setPrefWidth(8 * HEIGHT);
+        control.setFont(font);
+        control.setEditable(false);
+        GridPane.setMargin(control, new Insets(MARGIN_SIZE));
 
-        return button;
+        addControl(control, grid, "textWin", index);
+    }
+
+    private static void addBtnSpin(Font font, GridPane grid, int index) {
+        Button control = new Button("SPIN");
+        control.setFont(font);
+
+        addControl(control, grid, "btnSpin", index);
     }
 }
--- src/main/java/elements/Manage.java
+++ src/main/java/elements/Manage.java
@@ -1,11 +1,65 @@
 package elements;
 
 import javafx.application.Platform;
+import javafx.scene.control.Label;
 
 public class Manage {
 
+    private static final Integer[] BETS =
+            {1, 2, 3, 4, 5, 7, 10, 15, 20, 25, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 500, 750, 1000};
+
+    public static int bet;
+
+    static {
+        bet = BETS[0];
+    }
+
     public static void exitGame() {
         Platform.exit();
+    }
+
+    public static void currentBet(Label label, int value) {
+        int index = indexOf(BETS, value);
+        setBet(label, index);
+    }
+
+    private static void setBet(Label label, int index) {
+        if (index <= 0) {
+            index = 0;
+        }
+
+        bet = BETS[index];
+        double result = bet / 100.0;
+
+        label.setText(String.valueOf(result));
+    }
+
+
+    public static void minusBet(Label label) {
+        int index = indexOf(BETS, bet) - 1;
+        index = Math.max(index, 0);
+
+        setBet(label, index);
+    }
+
+    public static void plusBet(Label label) {
+        int index = indexOf(BETS, bet) + 1;
+        index = Math.min(index, BETS.length - 1);
+
+        setBet(label, index);
+    }
+
+    public static <T> int indexOf(T[] array, T value) {
+        int index = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
     }
 
 }
